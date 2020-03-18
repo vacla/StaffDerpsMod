0e19d7b05056cf2c49f4e8a8f6def2830c6c541b
Arnevdl29 <arnevdl@gmail.com>
2020-03-18 20:49:52 +0100

    added configurable tp command

--- src/main/java/eu/minemania/staffderpsmod/config/Configs.java
+++ src/main/java/eu/minemania/staffderpsmod/config/Configs.java
@@ -33,13 +33,15 @@ public class Configs implements IConfigHandler
         public static final ConfigBoolean SEE_PET_OWNER = new ConfigBoolean("seePetOwner", false, "If enabled, can see pet owner");
         public static final ConfigDouble SCALAR = new ConfigDouble("scalar", 2, 1, 9, "Sets how far a mob get summoned from where you look");
         public static final ConfigString SUMMON_COMMAND = new ConfigString("summonCommand", "/summon minecraft:pig ~ ~1 ~ {Attributes:[{Name:generic.maxHealth,Base:1}],Age:-99}", "Summon command");
+        public static final ConfigString TP_COMMAND = new ConfigString("tpCommand", "/tp", "Sets command that tp you");
 
         public static final ImmutableList<IConfigBase> OPTIONS = ImmutableList.of(
                 ENABLED,
                 SEE_INVISIBLE,
                 SEE_PET_OWNER,
                 SCALAR,
-                SUMMON_COMMAND
+                SUMMON_COMMAND,
+                TP_COMMAND
                 );
     }
 
